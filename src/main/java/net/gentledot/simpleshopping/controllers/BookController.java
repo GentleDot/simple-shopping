a8cfdac3096b4d451a2c20622402315f75193536
gentledot <gentledot.wp@gmail.com>
2019-12-29 23:47:30 +0900

    SimpleShopping_Book - BookController 구성_테스트 작성중

--- src/main/java/net/gentledot/simpleshopping/controllers/BookController.java
+++ src/main/java/net/gentledot/simpleshopping/controllers/BookController.java
@@ -1,6 +1,13 @@
 package net.gentledot.simpleshopping.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller(value = "bookController")
 public class BookController {
 
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
 }
---
+++ src/test/java/net/gentledot/simpleshopping/controllers/BookControllerTest.java
@@ -0,0 +1,35 @@
+package net.gentledot.simpleshopping.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+@AutoConfigureMockMvc
+public class BookControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void index() throws Exception {
+        // when
+        ResultActions actions = mockMvc.perform(get("/"));
+
+        // then
+        actions.andDo(print())
+                .andExpect(status().isOk());
+
+
+    }
+}
